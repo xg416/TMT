9ae97aa465924f63a6c7318d331c954b16c5e65d
XG Zhang <zzz1065965899@gmail.com>
2023-01-27 23:12:56 -0500

    Create video_inference.sh

---
+++ code/video_inference.sh
@@ -0,0 +1,9 @@
+python video_inference.py --march shuffle \
+    --patch_size 240 \
+    --temp_patch 12 \
+    --resize_ratio 1.0 \
+    --input_path video_22_all.avi \
+    --out_path video_22_out.mp4 \
+    --model_path ./model_zoo/shuffle_MS_video.pth \
+    --save_video \
+    --concatenate_input
